baab98e518f3f48530a9f187a6ce316b57005c1b
Tanaya-Dave <79378737+Tanaya-Dave@users.noreply.github.com>
2022-10-13 09:38:05 -0400

    Create vex.h

---
+++ vex.h
@@ -0,0 +1,9 @@
+#include "robot-config.h"
+
+#define waitUntil(condition)                                                   \
+  do {                                                                         \
+    wait(5, msec);                                                             \
+  } while (!(condition))
+
+#define repeat(iterations)                                                     \
+  for (int iterator = 0; iterator < iterations; iterator++)
